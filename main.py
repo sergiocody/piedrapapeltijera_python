c156492fb3bf6598f8b4371ce7c55634830d0b15
Cody <you@example.com>
2020-09-13 18:30:36 +0200

    init

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 #List of options
-game  = [Rock, Paper, Scissors]
+game  = ["Rock", "Paper", "Scissors"]
 
 #Assigning a random option to computer
 computer = game[randint(0,2)]
@@ -14,43 +14,43 @@ computersPoint = 0
 goOn = True
 
 #Loop goes on until goOn is false
-while(goOn)
+while(goOn):
     #Ask for user input
-    player = input(Rock, Paper or Scissors or enter Finish to end!n)
+    player = input("Rock, Paper or Scissors? or enter Finish to end!\n")
 
     #Check for scenarios
-    if(player == 'Finish')
+    if(player == 'Finish'):
         goOn = False
-    elif(player == computer)
-        print(Tie!)
-    elif(player == Rock)
-        if(computer == Paper)
-            print(You lose!, computer, covers, player)
+    elif(player == computer):
+        print("Tie!")
+    elif(player == "Rock"):
+        if(computer == "Paper"):
+            print("You lose!", computer, "covers", player)
             computersPoint = computersPoint + 1 
-        else
-            print(You win!, player, smashes, computer)
+        else:
+            print("You win!", player, "smashes", computer)
             playersPoint = playersPoint + 1
-    elif(player == Paper)
-        if(computer == Scissors)
-            print(You lose!, computer, cut, player)
+    elif(player == "Paper"):
+        if(computer == "Scissors"):
+            print("You lose!", computer, "cut", player)
             computersPoint = computersPoint + 1
-        else
-            print(You win!, player, covers, computer)
+        else:
+            print("You win!", player, "covers", computer)
             playersPoint = playersPoint + 1
-    elif(player == Scissors)
-        if(computer == Rock)
-            print(You lose..., computer, smashes, player)
+    elif(player == "Scissors"):
+        if(computer == "Rock"):
+            print("You lose...", computer, "smashes", player)
             computersPoint = computersPoint + 1
-        else
-            print(You win!, player, cut, computer)
+        else:
+            print("You win!", player, "cut", computer)
             playersPoint = playersPoint + 1
-    else
-        print(That's not a valid play. Check your spelling!)
+    else:
+        print("That's not a valid play. Check your spelling!")
     #Assigning a random option to computer
     computer = game[randint(0,2)]
-    print('Next Turn')
+    print('********Next Turn********')
 
 #Printing final points
-print(Final Points)
-print(Player , playersPoint)
-print(Computer , computersPoint)
+print("********Final Points********")
+print("Player: ", playersPoint)
+print("Computer: ", computersPoint)
